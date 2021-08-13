1fa13715ab778585d9246cfc7656bc064e61516d
Stephan Meighen-Berger <stephan.meighenberger@gmail.com>
2021-08-13 12:15:14 +0200

    Updated to make cluster compatible

---
+++ cluster/dmpoint_scan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /home/ga78fed/miniconda3/bin/activate
+# source "$HOME/.virtualenvs/AdS/bin/activate"
+# source /home/ga76zas/.virtualenvs/Ads/bin/activate
+# source /home/ga76zas/miniconda3/bin/activate
+# conda activate dmpoint
+python --version
+which python
+python $HOME/projects/dmpoint/cluster/env_check.py
+python $HOME/projects/dmpoint/cluster/dm_source_batch.py "${1}"
